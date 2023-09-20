d8da74fdb169ad50ad75858bef49615974dcf396
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-09-20 22:49:31 +0300

    create components

---
+++ StyledDiv.js
@@ -0,0 +1,10 @@
+import styled from './StyledDiv.css';
+
+<div className={products_list_container}>
+    <div className={products_list_container}>
+    <div className={styled.product_category}>
+        <div className={styled.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus sed viverra tellus in hac habitasse platea dictumst. Arcu felis bibendum ut tristique et. Id volutpat lacus laoreet non curabitur gravida arcu ac. Sed vulputate mi sit amet mauris commodo. Vel pretium lectus quam id leo. Tempor nec feugiat nisl pretium fusce id. Quam pellentesque nec nam aliquam sem et. Egestas maecenas pharetra convallis posuere morbi. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Habitasse platea dictumst quisque sagittis purus sit. Diam quam nulla porttitor massa id neque aliquam.</div>
+        <div className={styled.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus sed viverra tellus in hac habitasse platea dictumst. Arcu felis bibendum ut tristique et. Id volutpat lacus laoreet non curabitur gravida arcu ac. Sed vulputate mi sit amet mauris commodo. Vel pretium lectus quam id leo. Tempor nec feugiat nisl pretium fusce id. Quam pellentesque nec nam aliquam sem et. Egestas maecenas pharetra convallis posuere morbi. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Habitasse platea dictumst quisque sagittis purus sit. Diam quam nulla porttitor massa id neque aliquam.</div>
+    </div>
+    </div>
+</div>
